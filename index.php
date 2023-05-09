8b2954618356425040c1a11179c28126ce9e3ec7
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-05-09 16:28:18 +1200

    Updated the handlers

--- index.php
+++ index.php
@@ -1,40 +1,10 @@
 <?php
 
-require_once("./controllers/product.controller.php");
-require_once("./routers/router.php");
-
+require_once("./handlers/handlers.php");
 
 $app = new Router();
 
 
-function handleCreate()
-{
-    $product = new Product();
-    $product_name = $_POST['product_name'];
-    $product_description = $_POST['product_description'];
-    $product_price = $_POST['product_price'];
-    $product_image = $_FILES['product_image']['tmp_name'];
-
-    echo $product->create($product_name, $product_description, $product_price, $product_image);
-    die();
-}
-
-function getAllProducts()
-{
-    $product = new Product();
-
-    echo $product->get();
-    die();
-}
-
-function handleProductDelete($route_params)
-{
-    $product_id = $route_params['id'];
-    $product = new Product();
-
-    echo $product->delete($product_id);
-    die();
-}
 $app->route("POST", "/products/create", handleCreate());
 $app->route("GET", "/products/all", getAllProducts());
 $app->route("DELETE", "/products/delete{id", handleProductDelete($route_params));
