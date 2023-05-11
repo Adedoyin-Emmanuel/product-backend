80275968baf68150da1ff7549fbab2543418d631
Praisecodes <cmancoder@gmail.com>
2023-05-11 14:35:38 +0100

    [Fix] Attempt fix cors errors

--- routers/router.php
+++ routers/router.php
@@ -1,4 +1,9 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
 class Router
 {
